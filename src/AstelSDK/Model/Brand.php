9747e4011e7c6a2a546f8eacc2b0bd1f547d7188
Valentin Rocher <valouxxx@gmail.com>
2019-05-09 11:40:51 +0200

    #990 Testing #973 APIV2/Refactoring/SEO - Petites corrections REU 09/05/19

--- src/AstelSDK/Model/Brand.php
+++ src/AstelSDK/Model/Brand.php
@@ -1,9 +1,39 @@
 <?php
 
 namespace AstelSDK\Model;
+use CakeUtility\Hash;
 
 class Brand extends SDKModel {
 	
 	protected $associated_instance_name = '\AstelSDK\API\Brand';
+
+	/**
+	 * Get values for 5 stars quality score
+	 *
+	 * @param array $brand returned by API
+	 *
+	 * @return array of values for full Stars, Half stars and empty stars
+	 */
+	public function getQualityStarsValue(array $brand){
+		$score = 0;
+		$i = 0;
+		foreach(Hash::get($brand, 'quality_score', []) as $k => $play){
+			if((int)$play !== 0){
+
+				$score += (int) $play;
+				$i ++;
+			}
+		}
+		if($i > 0) {
+			// Get value on 5
+			$score = round($score / $i *0.05, 1);
+		}
+
+		$fullStars = floor($score);
+		$halfStars = ceil($score) - $fullStars;
+		$emptyStars = 5 - $fullStars - $halfStars;
+
+		return [$fullStars, $halfStars, $emptyStars];
+	}
 	
 }
