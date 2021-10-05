e5c704d42c16ca0ab6b774617b4dc81191fc0395
CharlesVanbeneden <charlesv@vanbe.be>
2021-10-05 09:44:23 +0200

    hotfix http error logs

--- src/AstelSDK/API/APIQuery.php
+++ src/AstelSDK/API/APIQuery.php
@@ -111,7 +111,7 @@ class APIQuery {
 		//curl_setopt($this->ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2_0);
 		curl_setopt($this->ch, CURLOPT_HTTPHEADER, $this->headers);
 		curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($this->ch, CURLOPT_VERBOSE, 1);
+		curl_setopt($this->ch, CURLOPT_VERBOSE, 0);
 		curl_setopt($this->ch, CURLOPT_HEADER, 1);
 		curl_setopt($this->ch, CURLOPT_URL, $this->lastUrl);
 	}
