8d9b6fafc64e8ddae77ff746cbe6f30c13efdeb8
michael <michael@tablebooker.be>
2022-03-30 16:34:49 +0200

    #2080 Add Cashbackclaim class to API model

---
+++ src/AstelSDK/API/CashbackClaim.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AstelSDK\API;
+
+use CakeUtility\Hash;
+
+class CashbackClaim extends APIModel {
+	
+	/**
+	 * @param array $data
+	 *
+	 * @return array
+	 * @throws \ValidationErrorException
+	 * @throws \AstelSDK\Exception\DataException
+	 * @throws \Exception
+	 */
+	public function createFirst(array $data = []) {
+		$query = $this->newQuery();
+		$query->setUrl('v2_00/cashback_claim/');
+		
+		$defaultData = [
+			'user_ip' => $this->context->getUserIP(),
+		];
+		$data = Hash::merge($defaultData, $data);
+		
+		$query->addPOSTParams($data);
+		
+		return $query->exec();
+	}
+}
