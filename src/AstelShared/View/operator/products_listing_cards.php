29377d38182a41d541a4f2c947d4b0c0e101952c
artedsolis <artedsolis@gmail.com>
2023-07-06 17:58:34 +0200

    #2746 -- Display cards with cashback

--- src/AstelShared/View/operator/products_listing_cards.php
+++ src/AstelShared/View/operator/products_listing_cards.php
@@ -2,31 +2,7 @@
 
 use CakeUtility\Hash;
 debug($params);
-
-
-
 ?>
-
-<h2>PRODUCT CARDS</h2>
-<div class="col-md-4">
-	<div class="row h-100">
-<!--		  --><?php
-//        foreach ($params['products'] as $product) {
-//					//debug($product['products']);
-//					debug($product['total_pricings']['quality_score']);
-//					//debug($product['name']);
-//			// Display the product name
-//
-//			 echo $product['total_pricings']['quality_score'];
-//			 echo $product['total_pricings']['total_price'];
-//
-//        }
-//
-//		?>
-	</div>
-</div>
-
-
 <!--<div class="row">-->
 <!--	--><?php
 //	foreach ($params['products'] as $product) {
@@ -64,17 +40,75 @@ debug($params);
 <!--	--><?php //} ?>
 <!--</div>-->
 
-<h3>PRODUCT CARDS VERSION 2</h3>
+<!--<h3>PRODUCT CARDS</h3>-->
+<!--<div class="row">-->
+<!--	--><?php //foreach ($params['products'] as $product) : ?>
+<!--		<div class="col-md-3">-->
+<!--			<div class="card mb-3">-->
+<!--				<div class="card-body">-->
+<!--					--><?php
+//					foreach ($product['products'] as $mobileProducts => $mobile) {
+//						$includedDataVolume = $mobile['play_description']['mobile']['included_data_volume'];
+//						$includedDataVolumeFormatted = round($includedDataVolume / 1024, 2);
+//						echo "<h2>{$mobile['brand_name']}</h2>";
+//						echo "<h2>GSM: {$includedDataVolumeFormatted} GB</h2>";
+//					}?>
+<!--					-->
+<!--					<h1>+</h1>-->
+<!--					-->
+<!--						--><?php
+//						foreach ($product['products'] as $productId => $productData) {
+//							echo "<h2>{$productData['brand_name']}</h2>";
+//							echo "<h3>Internet: {$productData['play_description']['internet']['bandwidth_volume']}</h3>";
+//							echo "<h3>TV: {$productData['play_description']['tv']['number_tv_channel']} chaînes</h3>";
+//						}
+//						?>
+<!--					-->
+<!--					<div>TOTAL: --><?//= $product['total_pricings']['total_price'] ?><!-- pendant XX mois</div>-->
+<!--					<div>puis --><?//= $product['total_pricings']['total_price_without_discount'] ?><!-- /mois TVAC</div>-->
+<!--				</div>-->
+<!--			</div>-->
+<!--		</div>-->
+<!--	--><?php //endforeach; ?>
+<!--</div>-->
+
+<h3>PRODUCT CARDS</h3>
 <div class="row">
 	<?php foreach ($params['products'] as $product) : ?>
 		<div class="col-md-3">
-			<div class="card">
+			<div class="card mb-3">
 				<div class="card-body">
-						<?php
-						foreach ($product['products'] as $productId => $productData) {
-							echo "<h2>{$productData['name']['FR']}</h2>";
+					<?php
+					$mobileBrandName = '';
+					foreach ($product['products'] as $mobileProducts => $mobile) {
+						
+						echo "<h2 class='float-right py-1 px-3' style='color:#fff; background-color: #f23078;'>€ {$mobile['commission']['cashback_amount']} cashback</h2><br>";
+						
+						$includedDataVolume = $mobile['play_description']['mobile']['included_data_volume'];
+						$includedDataVolumeFormatted = round($includedDataVolume / 1024, 2);
+						if ($mobileBrandName !== $mobile['brand_name']) {
+							echo "<h2 class='mt-4'>{$mobile['brand_name']}</h2>";
+							$mobileBrandName = $mobile['brand_name'];
 						}
-						?>
+						echo "<h2>GSM: {$includedDataVolumeFormatted} GB</h2>";
+					}
+					?>
+					<h1>+</h1>
+					<?php
+					$mobileBrandName = ''; // Initialize the variable outside the loop
+					foreach ($product['products'] as $productId => $productData) {
+						if ($mobileBrandName !== $productData['brand_name']) {
+							echo "<h2>{$productData['brand_name']}</h2>";
+							$mobileBrandName = $productData['brand_name'];
+						}
+						if ($productData['is_internet']) {
+							echo "<h3>Internet: {$productData['play_description']['internet']['bandwidth_volume']}</h3>";
+						}
+						if ($productData['is_tv']) {
+							echo "<h3>TV: {$productData['play_description']['tv']['number_tv_channel']} chaînes</h3>";
+						}
+					}
+					?>
 					<div>TOTAL: <?= $product['total_pricings']['total_price'] ?> pendant XX mois</div>
 					<div>puis <?= $product['total_pricings']['total_price_without_discount'] ?> /mois TVAC</div>
 				</div>
@@ -82,6 +116,8 @@ debug($params);
 		</div>
 	<?php endforeach; ?>
 </div>
+
+
 
 
 
