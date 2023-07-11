8adea37ea054cc664017adddf48c87e623caf039
artedsolis <artedsolis@gmail.com>
2023-07-11 15:57:50 +0200

    #2746 -- Display cards with merged cashbacks WIP

--- src/AstelShared/View/operator/products_listing_cards.php
+++ src/AstelShared/View/operator/products_listing_cards.php
@@ -1,77 +1,8 @@
 <?php
 
 use CakeUtility\Hash;
-debug($params);
+//debug($params);
 ?>
-<!--<div class="row">-->
-<!--	--><?php
-//	foreach ($params['products'] as $product) {
-//		//debug($product);
-//		?>
-<!--	<div class="col-md-3">-->
-<!--	<span>-->
-<!--		--><?php //if (Hash::get($product, 'products', false)) {
-//			foreach ($product['products'] as $productId => $productData) {
-//				echo "<h2>{$productData['name']['FR']}</h2>";
-//				//debug($productData);
-//			}
-//		}
-//		?>
-<!--	</span>-->
-<!--	<span>-->
-<!--    --><?php
-//		if (Hash::get($product, 'total_pricings.total_price', false)) {
-//			$totalPrice = $product['total_pricings']['total_price'];
-//			echo "<div>TOTAL : {$totalPrice} pendant XX mois</div>";
-//			//debug($product);
-//		}
-//		?>
-<!--	</span>-->
-<!--	<span>-->
-<!--    --><?php
-//		if (Hash::get($product, 'total_pricings.total_price_without_discount', false)) {
-//			$totalPriceWithoutDiscount = $product['total_pricings']['total_price_without_discount'];
-//			echo "<div>puis {$totalPriceWithoutDiscount} /mois TVAC</div>";
-//		}
-//		?>
-<!--	</span>-->
-<!--	</div>-->
-<!--	-->
-<!--	--><?php //} ?>
-<!--</div>-->
-
-<!--<h3>PRODUCT CARDS</h3>-->
-<!--<div class="row">-->
-<!--	--><?php //foreach ($params['products'] as $product) : ?>
-<!--		<div class="col-md-3">-->
-<!--			<div class="card mb-3">-->
-<!--				<div class="card-body">-->
-<!--					--><?php
-//					foreach ($product['products'] as $mobileProducts => $mobile) {
-//						$includedDataVolume = $mobile['play_description']['mobile']['included_data_volume'];
-//						$includedDataVolumeFormatted = round($includedDataVolume / 1024, 2);
-//						echo "<h2>{$mobile['brand_name']}</h2>";
-//						echo "<h2>GSM: {$includedDataVolumeFormatted} GB</h2>";
-//					}?>
-<!--					-->
-<!--					<h1>+</h1>-->
-<!--					-->
-<!--						--><?php
-//						foreach ($product['products'] as $productId => $productData) {
-//							echo "<h2>{$productData['brand_name']}</h2>";
-//							echo "<h3>Internet: {$productData['play_description']['internet']['bandwidth_volume']}</h3>";
-//							echo "<h3>TV: {$productData['play_description']['tv']['number_tv_channel']} chaînes</h3>";
-//						}
-//						?>
-<!--					-->
-<!--					<div>TOTAL: --><?//= $product['total_pricings']['total_price'] ?><!-- pendant XX mois</div>-->
-<!--					<div>puis --><?//= $product['total_pricings']['total_price_without_discount'] ?><!-- /mois TVAC</div>-->
-<!--				</div>-->
-<!--			</div>-->
-<!--		</div>-->
-<!--	--><?php //endforeach; ?>
-<!--</div>-->
-
 <h3>PRODUCT CARDS</h3>
 <div class="row">
 	<?php foreach ($params['products'] as $product) : ?>
@@ -80,35 +11,75 @@ debug($params);
 				<div class="card-body">
 					<?php
 					$mobileBrandName = '';
+					$cashbackTotal = 0; // Initialize cashback total
 					foreach ($product['products'] as $mobileProducts => $mobile) {
-						
-						echo "<h2 class='float-right py-1 px-3' style='color:#fff; background-color: #f23078;'>€ {$mobile['commission']['cashback_amount']} cashback</h2><br>";
-						
+						$includedMinutes = $mobile['play_description']['mobile']['included_minutes_calls'];
 						$includedDataVolume = $mobile['play_description']['mobile']['included_data_volume'];
-						$includedDataVolumeFormatted = round($includedDataVolume / 1024, 2);
-						if ($mobileBrandName !== $mobile['brand_name']) {
-							echo "<h2 class='mt-4'>{$mobile['brand_name']}</h2>";
-							$mobileBrandName = $mobile['brand_name'];
+						$includedDataVolumeFormatted = round($includedDataVolume / 1000, 1);
+						
+						if ($includedDataVolumeFormatted !== 0 && $includedDataVolumeFormatted !== null && $includedDataVolumeFormatted !== '') {
+							
+							// Add cashback amount to the total
+							$cashbackTotal += $mobile['commission']['cashback_amount'];
+							
+							// Output the merged cashback
+//							echo "<h2>Merged Cashback: € {$cashbackTotal}</h2>";
+						
+							// If I have more than one cashbacks , I need to merge them , so I output just one
+							echo "<h2 class='float-right py-1 px-3' style='color:#fff; background-color: #f23078;'>€ " . $cashbackTotal . " cashback</h2><br>";
+							
+							echo "<div class='border border-info p-3'>";
+							
+							// Output only if it's the first occurrence of the brand
+							if ($mobileBrandName !== $mobile['brand_name']) {
+								// if included data equals 0 , not show the brand name
+								if ($includedDataVolumeFormatted !== 0) {
+									echo "<h2 class='mt-4'>{$mobile['brand_name']}</h2>";
+									$mobileBrandName = $mobile['brand_name'];
+								}
+							}
+							
+							echo "<div>GSM: " . __d('CoreAstelBe', '%s GB', [$includedDataVolumeFormatted]) . "</div>";
+							
+							echo "<div>" . __d('CompareAstelBe', 'Included Minutes: %s', $mobile['play_description']['mobile']['included_minutes_calls']) . "</div>";
+							echo "<div>" . __d('CompareAstelBe', 'Included SMS: %s', $mobile['play_description']['mobile']['included_sms']) . "</div>";
+							echo "</div>";
 						}
-						echo "<h2>GSM: {$includedDataVolumeFormatted} GB</h2>";
 					}
 					?>
-					<h1>+</h1>
+					
+					<div class="d-flex justify-content-center mt-2">
+						<h1>+</h1>
+					</div>
+					
 					<?php
 					$mobileBrandName = ''; // Initialize the variable outside the loop
 					foreach ($product['products'] as $productId => $productData) {
-						if ($mobileBrandName !== $productData['brand_name']) {
-							echo "<h2>{$productData['brand_name']}</h2>";
-							$mobileBrandName = $productData['brand_name'];
-						}
-						if ($productData['is_internet']) {
-							echo "<h3>Internet: {$productData['play_description']['internet']['bandwidth_volume']}</h3>";
-						}
-						if ($productData['is_tv']) {
-							echo "<h3>TV: {$productData['play_description']['tv']['number_tv_channel']} chaînes</h3>";
+						
+						if ($includedDataVolumeFormatted !== 0 && $includedDataVolumeFormatted !== null && $includedDataVolumeFormatted !== '') {
+							
+							echo "<div class='border border-info p-3'>";
+							
+							if ($mobileBrandName !== $productData['brand_name']) {
+								echo "<h2>{$productData['brand_name']}</h2>";
+								$mobileBrandName = $productData['brand_name'];
+							}
+							if ($productData['is_internet']) {
+								echo "<h3>Internet: {$productData['play_description']['internet']['bandwidth_volume']}</h3>";
+							}
+							if ($productData['is_tv']) {
+								echo "<h3>TV: {$productData['play_description']['tv']['number_tv_channel']} chaînes</h3>";
+							}
+							
+							echo "</div>";
 						}
 					}
 					?>
+					
+					<div class="d-flex justify-content-center mt-2">
+						<h1>=</h1>
+					</div>
+					
 					<div>TOTAL: <?= $product['total_pricings']['total_price'] ?> pendant XX mois</div>
 					<div>puis <?= $product['total_pricings']['total_price_without_discount'] ?> /mois TVAC</div>
 				</div>
