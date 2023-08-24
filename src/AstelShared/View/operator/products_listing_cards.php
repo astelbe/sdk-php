78714c4cb860fa2d06169433c631a6896240322d
artedsolis <artedsolis@gmail.com>
2023-08-24 13:47:07 +0200

    #2746 WIP -- display with grids

--- src/AstelShared/View/operator/products_listing_cards.php
+++ src/AstelShared/View/operator/products_listing_cards.php
@@ -24,18 +24,45 @@ $toggle = '<div class="d-flex align-items-center mr-3 text-lighter text-uppercas
 											style="right:0; z-index:999">' . __d('CompareAstelBe', 'switch_resume') . '<input class="mx-2 switch-toggle-details" id="toggle-details-' . $key  . '" type="checkbox">' . __d('CompareAstelBe', 'switch_details') . '</div>';
 ?>
 
+<style>
+	.grid-container {
+		display: grid;
+		grid-template-columns: repeat(4, auto);
+		/*gap: 6px;*/
+	}
+	
+	.grid-container-internet {
+		display: grid;
+		grid-template-columns: repeat(3, auto);
+		/*gap: 6px;*/
+	}
+	
+	.grid-container-tv {
+		display: grid;
+		grid-template-columns: repeat(3, auto);
+		gap: 63px;
+	}
+	
+	.grid-container-fix {
+		display: grid;
+		grid-template-columns: repeat(3, auto);
+		/*gap: 0px;*/
+	}
+</style>
+
 <div class="container mt-5">
 	<?=$toggle;?>
 	<div class="row">
 	<?php
 	foreach ($params as $key => $result) {
 	?>
-		<div class="col-md-4 mb-5">
+		<div class="col-md-4 mb-5 align-self-lg-start">
+<!--		<div class="col-md-4 mb-5">-->
 			<div class="px-3 py-4 shadow rounded-lg d-flex h-100 flex-column justify-content-lg-between">
 				<div class="mt-n5 ml-3 py-2 px-2 position-relative rounded-sm" style="color:#fff; background-color: #f23078; left: 160px; width: 160px; font-size: 1.125rem;">
 				<?= $result['total_pricings']['total_cashback']?>
 				</div>
-					<div class="">
+					<div class="" style="">
 						<?php
 						foreach ($result['products'] as $key => $item) {
 //							debug($item);
@@ -49,11 +76,15 @@ $toggle = '<div class="d-flex align-items-center mr-3 text-lighter text-uppercas
 											//mobile
 											if ($item['mobile'] !== false){
 											?>
-												<p>GSM:</p>
-												<li><?= $item['mobile']['included_data_volume']; ?></li>
-												<li><?= $item['mobile']['included_sms'] ?></li>
-												<li><?= $item['mobile']['included_minutes_calls'] ?></li>
-												<?= $item['mobile']['price_description'] ?>
+<!--												<div class="grid-container">-->
+												<div class="">
+													<div class="label">GSM:</div>
+<!--													<div><strong>--><?//= $item['mobile']['included_data_volume']; ?><!--</strong></div>-->
+<!--													<div>--><?//= $item['mobile']['included_minutes_calls'] ?><!--</div>-->
+<!--													<div>--><?//= $item['mobile']['included_sms'] ?><!--</div>-->
+													<div><strong><?= $item['mobile']['included_data_volume']; ?></strong><?= $item['mobile']['included_minutes_calls'] ?><?= $item['mobile']['included_sms'] ?></div>
+												</div>
+													<?= $item['mobile']['price_description']; ?>
 											<?php
 											} else {
 												false;
@@ -61,10 +92,12 @@ $toggle = '<div class="d-flex align-items-center mr-3 text-lighter text-uppercas
 											// internet
 											if ($item['internet'] !== false){
 											?>
-												<p>Internet:</p>
-												<li><?= $item['internet']['bandwidth_download']; ?></li>
-												<li><?= $item['internet']['bandwidth_volume']; ?></li>
-												<?= $item['internet']['price_description'] ?>
+												<div class="grid-container-internet">
+													<div class="label mt-2">Internet: </div>
+													<div class="mt-2"><strong><?=$item['internet']['bandwidth_download']; ?></strong></div>
+													<div class="mt-2"><?= $item['internet']['bandwidth_volume']; ?></div>
+												</div>
+													<?= $item['internet']['price_description']; ?>
 											<?php
 											} else {
 												false;
@@ -72,27 +105,41 @@ $toggle = '<div class="d-flex align-items-center mr-3 text-lighter text-uppercas
 											// tv
 											if ($item['tv'] !== false){
 											?>
-												<p class="mt-3">TV:</p>
-<!--												--><?php //debug($item['tv'])?>
-												<li><?= $item['tv']['number_tv_channel']; ?></li>
-												<?php if ($item['tv']['decoder_application']) {?>
-												<li><?= $item['tv']['decoder_application']; ?></li>
-												<?php  } elseif ($item['tv']['decoder_only']) { ?>
-												<li><?= $item['tv']['decoder_only']; ?></li>
-												<?php  } else  { ?>
-												<li><?= $item['tv']['application_only']; ?></li>
-												<?= $item['tv']['price_description'] ?>
+												<div class="grid-container-tv">
+													<div class="label mt-2">TV: </div>
+													<div class="mt-2">
+														<strong><?= $item['tv']['number_tv_channel']; ?></strong>
+													</div>
+													<?php if ($item['tv']['decoder_application']) {?>
+													<div class="mt-2">
+														<?= $item['tv']['decoder_application']; ?>
+													</div>
+													<?php  } elseif ($item['tv']['decoder_only']) { ?>
+													<div class="mt-2">
+														<?= $item['tv']['decoder_only']; ?>
+													</div>
+													<?php  } else  { ?>
+													<div>
+														<?= $item['tv']['application_only']; ?>
+													</div>
+													}
+												<?php } ?>
+												</div>
+												<?= $item['tv']['price_description'];?>
 											<?php
-												}
 											} else {
 												false;
 											}
 											// fix
 											if ($item['fix'] !== false){
 													?>
-													<p class="mt-3">FIX:</p>
-												<?php if ($item['fix']['included_minutes_calls'] !== null) {?>
-													<li><?= $item['fix']['included_minutes_calls']; ?></li>
+												<div class="grid-container-fix">
+													<div class="label mt-2">FIX:</div>
+														<?php if ($item['fix']['included_minutes_calls'] !== null) {?>
+													<div class="mt-2">
+														<?= $item['fix']['included_minutes_calls']; ?>
+													</div class="mt-2">
+												</div>
 													<?= $item['fix']['price_description'] ?>
 													<?php } ?>
 													<?php
@@ -108,7 +155,7 @@ $toggle = '<div class="d-flex align-items-center mr-3 text-lighter text-uppercas
 					<div class="results-wrapper">
 						<div class="results-price">
 							<?=$result['total_pricings']['quality_score'];?>
-								<div>
+								<div class="">
 									<?=$result['total_pricings']['total_price'];?>
 									<?=$result['total_pricings']['total_price_without_discount'];?>
 								</div>
