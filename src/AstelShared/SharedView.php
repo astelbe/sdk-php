4a72fad11545bdbe24754afa8ee1ca955f261780
valouxxx <valouxxx@gmail.com>
2023-09-14 16:59:32 +0200

    #2746 review + refactor partiel

--- src/AstelShared/SharedView.php
+++ src/AstelShared/SharedView.php
@@ -7,10 +7,21 @@ use AstelSDK\Model\Product;
 use CakeUtility\Hash;
 
 class SharedView extends Singleton {
-	
-	public function render($path, $params = []) {
 
+
+	public $language = 'fr';
+	public $version = 'front'; // 'cake' or 'front'
+
+	public function render($path, $params = []) {
 		include __DIR__ . '/../AstelShared/View/' . $path . '.php';
+	}
+
+	public function setLanguage ($language) {
+		$this->language = $language;
+	}
+
+	public function setVersion($version) {
+		$this->version = $version;
 	}
 
 	/**
@@ -213,5 +224,174 @@ class SharedView extends Singleton {
 		return $html;
 
 	}
+
+	/**
+	 * @param $product
+	 * Process plays details for listings_cards
+	 */
+	public function getPlayDetails($item, $version = 'front', $language = 'fr') {
+		$tv_content = '<span style="font-size: 1.25rem; color:#1f438c!important;">
+												<strong>' . $item['tv']['number_tv_channel'] . '</strong>
+										</span>';
+		$tv_content .= '<span class="mr-1">';
+		foreach (['decoder_application', 'decoder_only', 'application_only'] as $decoder_type) {
+			if ($item['tv'][$decoder_type]) {
+				$tv_content .= $item['tv'][$decoder_type];
+			}
+		}
+		$tv_content .= '</span>';
+
+
+		$details = [
+			'mobile' => [
+				'key' => 'mobile',
+				'title' => 'GSM',
+				'description' => $item['mobile']['price_description'],
+				'content' => $this->getGsmDetails($item, $version, $language),
+//				'content' => '<span>
+//					<strong style="font-size: 1.25rem; color:#1f438c!important;">' . $item['mobile']['included_data_volume'] . '</strong>
+//						</span>
+//						<span class="mr-1">
+//										' . $item['mobile']['included_minutes_calls'] . '
+//						</span>
+//						<span class="mr-1">
+//										' . $item['mobile']['included_sms'] . '
+//						</span>',
+			],
+			'internet' => [
+				'key' => 'internet',
+				'title' => 'NET',
+				'description' => $item['internet']['price_description'] . '<br>',
+				'content' => '<span>Vitesse <strong class="mt-n1" style="font-size: 1.25rem; color:#1f438c!important;">' . $item['internet']['bandwidth_download'] . '</strong>
+                                                                            <span class="mr-1">' . $item['internet']['bandwidth_volume'] . '</span>
+                                                                        </span>',
+			],
+			'tv' => [
+				'key' => 'tv',
+				'title' => 'TV',
+				'description' => $item['tv']['price_description'],
+				'content' => $tv_content,
+			],
+			'fix' => [
+				'key' => 'fix',
+				'title' => 'TEL',
+				'description' => $item['fixed']['price_description'],
+				'content' => '<span>' . $item['fix']['included_minutes_calls'] . '</span>',
+			],
+		];
+
+		return $details;
+	}
+
+	public function getGsmDetails ($product) {
+		$Product = Product::getInstance();
+		if ($Product->isType($product, 'M')) {
+			$details = [];
+			$details[] = $this->translatePlayDescription('play_description.mobile.included_data_volume', $product);
+			$details[] = $this->translatePlayDescription('play_description.mobile.included_sms', $product);
+			$details[] = $this->translatePlayDescription('play_description.mobile.included_minutes_calls', $product);
+			$return_details = implode(', ', $details);
+			return [
+				'details' => $return_details,
+				'descriptions' => Hash::get($product, 'play_description.mobile.price_description.'.$this->language),
+				'label' => 'GSM'
+			];
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * @param $playDescriptionPath in $product array
+	 * @param $product
+	 * @param string $version. 'front' or 'cake', to get the domain name used in translation keys
+	 * @param null $responsive
+	 *
+	 * @return null|string $description
+	 */
+	public function translatePlayDescription($playDescriptionPath, $product) {
+		$description = Hash::get($product, $playDescriptionPath, null);
+		if (!$description) {
+			return null;
+		}
+		if($this->version == 'front') {
+			$translation_domain = 'product';
+		} else {
+			$translation_domain = 'OrderAstelBe';
+		}
+
+		switch ($playDescriptionPath) {
+
+			// MOBILE
+			case 'play_description.mobile.included_minutes_calls' :
+				if ($description == 'UNLIMITED') {
+					return self::getTranslation($translation_domain, 'tab_mobile_unlimited_call', $this->version);
+				} else {
+					return self::getTranslation($translation_domain, 'tab_mobile_minutes', $this->version, $description);
+				};
+				break;
+			case 'play_description.mobile.included_data_volume' :
+				if ($description == 'UNLIMITED') {
+					return self::getTranslation($translation_domain,'tab_mobile_unlimited_internet', $this->version);
+				} else {
+					return self::getTranslation($translation_domain,'tab_mobile_gb_data', $this->version,$description / 1000);
+				}
+				break;
+			case 'play_description.mobile.included_sms' :
+				if ($description == 'UNLIMITED') {
+					return self::getTranslation($translation_domain, 'tab_mobile_unlimited_sms', $this->version);
+				} else {
+					return self::getTranslation($translation_domain, 'tab_mobile_sms', $this->version, $description);
+				}
+				break;
+			// INTERNET
+			case 'play_description.internet.bandwidth_download' :
+				return self::getTranslation($translation_domain, 'tab_internet_mbps', $this->version, $description);
+				break;
+			case 'play_description.internet.bandwidth_upload' :
+				return self::getTranslation($translation_domain, 'tab_internet_mbps', $this->version, $description);
+				break;
+			case 'play_description.internet.bandwidth_volume' :
+				if ($description == 'UNLIMITED') {
+					return self::getTranslation($translation_domain, 'tab_unlimited', $this->version);
+				} else {
+					return self::getTranslation($translation_domain, 'tab_mobile_gb_data', $this->version, $description);
+				}
+			// FIX
+			case 'play_description.fix.included_minutes_calls' :
+				if ($description == 'UNLIMITED') {
+//					return d__('product', 'tab_mobile_unlimited_call');
+					return self::getTranslation($translation_domain, 'tab_mobile_unlimited_call', $this->version);
+				} else if ($description == 'EWE') {
+//					return d__('product', 'tab_fixe_EWE');
+					return self::getTranslation($translation_domain, 'tab_fixe_EWE', $this->version);
+				} else {
+					if ($description == 0) {
+						return '/';
+					}
+//					return d__('product', 'tab_mobile_minutes' . $responsive, $description);
+					return self::getTranslation($translation_domain, 'tab_mobile_minutes', $this->version, $description);
+				}
+			// TV : info has no translation keys with data injected
+			case 'play_description.tv.number_tv_channel' :
+				return self::getTranslation($translation_domain, 'tab_tv_number_of_channel', $this->version, $description);
+				break;
+			case 'play_description.tv.max_tv_channel' :
+				return self::getTranslation($translation_domain, 'tab_max_tv_channel', $this->version, $description);
+				break;
+			case 'play_description.tv.decoder_application' :
+				return self::getTranslation($translation_domain, 'tab_tv_decoder_application', $this->version, $description);
+				break;
+			case 'play_description.tv.tab_tv_decoder_only':
+				return self::getTranslation($translation_domain, 'tab_tv_decoder_only', $this->version, $description);
+				break;
+			case 'play_description.tv.application_only' :
+				return self::getTranslation($translation_domain, 'tab_tv_application_without_decoder', $this->version, $description);
+				break;
+			default:
+				return $description;
+		}
+	}
+
 }
 
--- src/AstelShared/View/operator/products_listing_cards.php
+++ src/AstelShared/View/operator/products_listing_cards.php
@@ -1,235 +1,113 @@
 <?php
-
 use CakeUtility\Hash;
-//debug($params);
-//debug($params['products']);
 /*
 	This template received $params :	array of results with products and pricings
-	$params =	[
-		'products' => [
-			[
-				'brand_name' => 'Proximus',
-				'short_name' => 'Tuttimus',
-				'...' => ...,
-			],
-		],
-		'total_pricings' => [
-				'price' => 99,
-				'...' => ...,
-		],
-	];
+	$params = [
+        0 =>[
+            'products' => [
+                [
+                    'brand_name' => 'Proximus',
+                    'short_name' => 'Tuttimus',
+                    '...' => ...,
+                ],
+            ],
+            'result_summary' => [
+                    'price' => 99,
+                    '...' => ...,
+            ],
+        ],
+        ...
+    ];
 */
 
-$toggle = '<div class="d-flex align-items-center mr-3 text-lighter text-uppercase"
-											style="right:0; z-index:999">' . __d('CompareAstelBe', 'switch_resume') . '<input class="mx-2 switch-toggle-details" id="toggle-details-' . $key  . '" type="checkbox">' . __d('CompareAstelBe', 'switch_details') . '</div>';
-
-
 ?>
-
-<style>
-	/*.label {*/
-	/*	font-size: 0.875rem;*/
-	/*}*/
-	
-	/*tr {*/
-	/*	vertical-align: top;*/
-	/*}*/
-</style>
 
 <div class="container mt-5">
-	<?=$toggle;?>
-	<div class="row mt-4">
-	<?php
-	foreach ($params as $key => $result) {
-	?>
-<!--		<div class="col-md-4 mb-5 align-self-lg-start">-->
-		<div class="col-md-3 mb-5">
-				<div class="mt-n3 ml-3 py-2 pl-4 position-absolute rounded-sm" style="color:#fff; background-color: #f23078; left: 95px; width: 160px; font-size: 1.125rem;">
-				<?= $result['total_pricings']['total_cashback']?>
-				</div>
-			<div class="px-3 py-4 shadow rounded-lg d-flex h-100 flex-column justify-content-between">
-<!--			<div class="px-3 py-4 shadow rounded-lg h-100">-->
-					<div class="mt-2">
-						<?php
-						foreach ($result['products'] as $key => $item) {
-							
-							$tv_content = '<span style="font-size: 1.25rem; color:#1f438c!important;">
-														<strong>'.$item['tv']['number_tv_channel'].'</strong>
-						</span>';
-						
-						if ($item['tv']['decoder_application']) {
-							$tv_content .= '<span class="mr-1">
-															'.$item['tv']['decoder_application'].'
-														</span>';
-						} elseif ($item['tv']['decoder_only']) {
-							
-							$tv_content .= '<span class="mr-1">
-															'.$item['tv']['decoder_only'].'
-														</span>';
-						} elseif ($item['tv']['application_only']) {
-							
-							$tv_content .= '<span class="mr-1">
-															'.$item['tv']['application_only'].'
-														</span>';
-						}
-							
-							
-							$tv_content2 = '<span style="font-size: 1.25rem; color:#1f438c!important;">
-														<strong>'.$item['tv']['number_tv_channel'].'</strong>
-						</span><br>';
-							
-							if ($item['tv']['decoder_application']) {
-								$tv_content2 .= '<span class="mr-1">
-															'.$item['tv']['decoder_application'].'
-														</span><br>';
-							} elseif ($item['tv']['decoder_only']) {
-								
-								$tv_content2 .= '<span class="mr-1">
-															'.$item['tv']['decoder_only'].'
-														</span><br>';
-							} else {
-								
-								$tv_content2 .= '<span class="mr-1">
-															'.$item['tv']['application_only'].'
-														</span>';
-							}
-							
-							
-							$details = [
-								'mobile' => [
-									'key' => 'mobile',
-									'title' => 'GSM',
-									'description' => $item['mobile']['price_description'],
-									'content' => '<span>
-													<strong style="font-size: 1.25rem; color:#1f438c!important;">'. $item['mobile']['included_data_volume'].'</strong>
-														</span>
-														<span class="mr-1">
-																'.$item['mobile']['included_minutes_calls'].'
-														</span>
-														<span class="mr-1">
-																' . $item['mobile']['included_sms']. '
-														</span>',
-								],
-								'internet' => [
-									'key' => 'internet',
-									'title' => 'NET',
-									'description' => $item['internet']['price_description']. '<br>' ,
-									'content' => '<span>Vitesse <strong class="mt-n1" style="font-size: 1.25rem; color:#1f438c!important;">'. $item['internet']['bandwidth_download'].'</strong>
-																	<span class="mr-1">'. $item['internet']['bandwidth_volume'].'</span>
-																</span>',
-								],
-								'tv' => [
-									'key' => 'tv',
-									'title' => 'TV',
-									'description' => $item['tv']['price_description'],
-									'content' => $tv_content,
-								],
-								'fix' => [
-									'key' => 'fix',
-									'title' => 'TEL',
-									'description' => $item['fixed']['price_description'],
-									'content' => '<span>'.$item['fix']['included_minutes_calls'].'</span>',
-								],
-							];
-							
-							$details2 = [
-								'mobile' => [
-									'key' => 'mobile',
-									'title' => 'GSM',
-									'description' => $item['mobile']['price_description'],
-									'content' => '<span>
-													<strong style="font-size: 1.25rem; color:#1f438c!important;">'. $item['mobile']['included_data_volume'].'</strong>
-														</span><br>
-														<span class="mr-1">
-																'.$item['mobile']['included_minutes_calls'].'
-														</span><br>
-														<span class="mr-1">
-																' . $item['mobile']['included_sms']. '
-														</span>',
-								],
-								'internet' => [
-									'key' => 'internet',
-									'title' => 'NET',
-									'description' => $item['internet']['price_description'],
-									'content' => '<span>Vitesse <strong class="mt-n1" style="font-size: 1.25rem; color:#1f438c!important;">'. $item['internet']['bandwidth_download'].'</strong><br>
-																	<span class="mr-1">'. $item['internet']['bandwidth_volume'].'</span>
-																</span>',
-								],
-								'tv' => [
-									'key' => 'tv',
-									'title' => 'TV',
-									'description' => $item['tv']['price_description'],
-									'content' => $tv_content2,
-								],
-								'fix' => [
-									'key' => 'fix',
-									'title' => 'TEL',
-									'description' => $item['fixed']['price_description'],
-									'content' => '<span>'.$item['fix']['included_minutes_calls'].'</span>',
-								],
-							];
-//							debug($item['mobile']);
 
+    <div class="d-flex align-items-center mr-3 text-lighter text-uppercase" style="right:0; z-index:999">
+      <?= __d('CompareAstelBe', 'switch_resume')?>
+        <input class="mx-2 switch-toggle-details" id="toggle-details-<?= $key ?>" type="checkbox">
+      <?= __d('CompareAstelBe', 'switch_details') ?>
+    </div>
 
+    <div class="row mt-4">
+        <?php foreach ($params['results'] as $key => $result) { ?>
+            <div class="col-md-3 mb-5">
+                <div class="mt-n3 ml-3 py-2 pl-4 cursor-pointer position-absolute rounded-sm plugin-hidden-optional-element cashback-amount modalClick "
+                    data-toggle="modal"
+                    data-target="#pluginModalCashback"
+                    style="color:#fff; background-color: #f23078; left: 95px; width: 160px; font-size: 1.125rem;"
+                >
+                    <?= $result['result_summary']['total_cashback']?>
+                </div>
+                <div class="px-3 py-4 shadow rounded-lg d-flex h-100 flex-column justify-content-between">
+                        <div class="mt-2">
+                            <?php
+                            foreach ($result['products'] as $key => $item) {
+                                $details = self::getPlayDetails($item)
+    //							?>
+                                    <div class="pb-3 mb-2 rounded" style="background-color: #f5f5f5">
+                                        <div>
+                                            <h2 class="pt-1 px-1 text-<?= $item['brand_slug']; ?>" style="font-size:1.5rem"><?= $item['brand_name']; ?></h2>
+                                            <h3 class="px-1 text-<?= $item['brand_slug']; ?>" style="min-height: 46px; font-size: 1.1rem; color: #f23078"><?= $item['short_name']; ?></h3>
+                                        </div>
+                                        <div class="pt-1 px-1">
+                                                <?php
 
-//							?>
-								<div class="pt-1 pb-3 pl-2 pr-1 mb-2 rounded" style="background-color: #f5f5f5">
-									<div>
-										<h2 class="" style="font-size:1.1rem"><?= $item['brand_name']; ?></h2>
-										<p class="text-uppercase" style="font-size: 1.1rem; color: #f23078"><?= $item['short_name']; ?></p>
-									</div>
-									<div>
-											<?php
-											
-											foreach ($details as $k => $play) {
-												if ($item[$k] !== false){ ?>
-													<div class="d-flex align-items-baseline mb-2" style="line-height:25px">
-														<div class="mr-2">
-															<span style="display:inline-block; width:35px"><?= $play['title']?></span>
-														</div>
-														<div>
-															<?= $play['content']?>
-														</div>
-													</div>
-														<?= $play['description']?>
-												<?php
-												}
-											}
+                                                foreach ($item['plays'] as $k => $play) {
+                                                    if ($item[$k] !== false){ ?>
+                                                        <div class="d-flex align-items-baseline mb-2" style="line-height:25px;font-size:0.875rem;">
+                                                            <div class="mr-2">
+                                                                <span style="display:inline-block; width:35px"><?= $play['label']?></span>
+                                                            </div>
+                                                            <div>
+                                                                <?= $play['details']?>
+                                                            </div>
+                                                        </div>
+                                                        <p class="sub-details-infos toggle-details toggle-details-'. $key .'">
+                                                            <?= $play['description']?>
+                                                        </p>
+                                                    <?php
+                                                    }
+                                                }
 
-											?>
-										</div>
-								</div>
-						<?php
-						}
-					?>
-					</div>
-					<div class="results-wrapper text-center">
-						<div class="results-price d-flex flex-column justify-content-center">
-							<div class="">
-								<?=$result['total_pricings']['quality_score'];?>
-							</div>
-								<div class="">
-									<?=$result['total_pricings']['total_price'];?>
-									<?=$result['total_pricings']['total_price_without_discount'];?>
-								</div>
-								<div class="setup-wrapper">
-									<p>
-									<p class="mb-n1 font-weight-bold" style="font-size: 1.2rem">Activation et Installation</p>
-									<?=$result['total_pricings']['reduced_total_setup_price'];?>
-									<?=$result['total_pricings']['full_total_setup_price'];?>
-									</p>
-								</div>
-								<?=$result['total_pricings']['products_total_savings']?>
-								<div class="pt-4 mt-5" style="margin-top: 120px;">
-									<?=$result['total_pricings']['order_url'];?>
-								</div>
-						</div>
-					</div>
-			</div>
-		</div>
-<?php
-}
-?>
+                                                ?>
+                                            </div>
+                                    </div>
+                            <?php
+                            }
+                        ?>
+                        </div>
+                        <div class="results-wrapper text-center">
+                            <div class="results-price d-flex flex-column justify-content-center">
+                                <div class="cursor-pointer modalClick" data-toggle="modal" data-target="#modalQuality" style="display:inline-block">
+                                    <?=$result['result_summary']['quality_score'];?>
+                                    <span class="cursor-pointer position-absolute">
+                                        <i class="fa fa-info pl-2"></i>
+                                     </span>
+                                </div>
+                                    <div class="">
+                                        <?=$result['result_summary']['total_price'];?>
+                                        <?=$result['result_summary']['total_price_without_discount'];?>
+                                    </div>
+                                    <div class="setup-wrapper">
+                                        <p>
+                                        <p class="mb-n1 font-weight-bold" style="font-size: 1.2rem">Activation et Installation</p>
+                                        <?=$result['result_summary']['reduced_total_setup_price'];?>
+                                        <?=$result['result_summary']['full_total_setup_price'];?>
+                                        </p>
+                                    </div>
+                                    <?=$result['result_summary']['products_total_savings']?>
+                                    <div class="pt-4 mt-5" style="margin-top: 120px;">
+                                        <?=$result['result_summary']['order_url'];?>
+                                    </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+    <?php
+    }
+        ?>
 	</div>
 </div>
 
