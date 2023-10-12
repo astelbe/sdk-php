8dde2c8d3bc0514558c20f113460ad5d6eec7d20
valouxxx <valouxxx@gmail.com>
2023-10-12 10:05:08 +0200

    #2746 review design

--- src/AstelShared/View/operator/products_listing_cards.php
+++ src/AstelShared/View/operator/products_listing_cards.php
@@ -22,49 +22,65 @@ use CakeUtility\Hash;
 ?>
 
 <div class="container px-0">
+
+    <div class="d-flex justify-content-end mb-5">
+        <div class="btn btn-outline-secondary text-uppercase switch-toggle-details cursor-pointer" id="toggle-details-<?= $params['id'] ?>">
+            <div class="details-hidden">
+                <?= __d('CompareAstelBe', 'switch_resume')?> <i class="fa fa-chevron-up ml-2" aria-hidden="true"></i>
+            </div>
+            <div class="details-shown d-none">
+                <?= __d('CompareAstelBe', 'switch_details') ?> <i class="fa fa-chevron-down ml-2" aria-hidden="true"></i>
+            </div>
+        </div>
+    </div>
+
+
     <div class="row mt-4 no-gutters">
         <?php foreach ($params['results'] as $key => $result) {
             ?>
-            <div class="col-12 col-xl-3 col-lg-4 col-md-6 mb-5 px-1 mb-5 mt-2 product-card">
-							<div class="result-index">
-								<?= $key += 1; ?>
-							</div>
-                <div class="px-2 py-4 shadow rounded-lg d-flex h-100 flex-column justify-content-between">
-                    <div class="mt-n3 ml-3 py-1 px-4 shadow cursor-pointer position-absolute rounded-sm plugin-hidden-optional-element cashback-amount modalClick "
+            <div class="col-12 col-xl-3 col-lg-4 col-md-6 mb-5 px-1 mb-5 mt-4 product-card">
+                <?php if($result['result_index']) { ?>
+                    <div class="result-index ml-2">
+                        <?= $result['result_index'] ?>
+                    </div>
+                <?php } ?>
+                <div class="px-2 pt-4 pb-2 rounded-lg d-flex h-100 flex-column justify-content-between" style="box-shadow: 2px 0rem 1.2rem rgba(0,0,0,.35)!important">
+                    <div class="mt-n3 ml-3 py-0 px-3 shadow cursor-pointer position-absolute rounded-sm plugin-hidden-optional-element cashback-amount modalClick "
                         data-toggle="modal"
                         data-target="#pluginModalCashback"
-                        style="color:#fff; background-color: #f23078; top:-8px; right: 0.75rem; font-size: 0.9rem;"
+                        style="color:#fff; background-color: #f23078; top:2px; height:32px; line-height: 32px; right: 0.75rem; font-size: 0.9rem;"
                     >
-                        <?= $result['result_summary']['total_cashback']?>
+                        <?= $result['result_summary']['total_cashback']?> <i class="fa pl-3 fa-info-circle" style="font-size:1rem"></i>
                     </div>
-                    <div class="mt-2">
+                    <div class="mt-3">
                         <?php
                         $cpt = 1; // To display "+"
                         foreach ($result['products'] as $key => $item) {
- 													// Display "+" between products
                             if($cpt > 1) { ?>
-                                <div class="w-100 text-center mb-2">
-                                    <i class="fa fa-plus" style="color:#c1c1c1" aria-hidden="true"></i>
+                                <div class="w-100 text-center mb-1">
+                                    <i class="fa fa-plus" style="color:#878787" aria-hidden="true"></i>
                                 </div>
-                            <?php }
-                            $cpt++;
-                            ?>
-                            <div class="mb-2 rounded" style="background-color: #f5f5f5">
-                                <h2 class="pb-1 pt-2 px-1 d-flex justify-content-between text-white rounded-top  bg-<?= $item['brand_slug']; ?>" style="font-size:1.5rem">
-                                  <?= $item['brand_name']; ?>
-                                    <span class=""><?= self::getDisplayedProductCount($item) ?></span>
-																	
+                            <?php } ?>
+                            <div class="mb-0 pb-0 rounded" style="background-color: #f5f5f5">
 
+                                <?php
+                                // Display brand name only if 1st product , and also 2dn result if multi brand result
+                                if($cpt == 1 || ($cpt == 2 && $params['id'] == 'view_multi_brand')) { ?>
+                                <h2 class="p-2 mb-0 text-center text-white rounded-top  bg-<?= $item['brand_slug']; ?>" style="font-size:1.2rem">
+                                  <?= $item['brand_name']; ?>
+<!--                                    <span class="">--><?//= self::getDisplayedProductCount($item) ?><!--</span>-->
                                 </h2>
-                                <h3 class="px-1" style="min-height: 46px; font-size: 1.1rem;">
-                                  <span class="text-<?= $item['brand_slug']; ?>">
-                                    <?= $item['short_name']; ?>
-                                  </span>
+                                <?php } ?>
+                                <h3 class="px-1 pt-3 d-flex justify-content-between" <?= ($cpt == 1 ? 'style="min-height: 46px; font-size: 1.1rem;"' : '') ?>>
+                                    <span class="text-<?= $item['brand_slug']; ?>">
+                                        <?= $item['short_name']; ?>
+                                    </span>
+                                    <span class="font-weight-bold" style="1.2rem;"><?= self::getDisplayedProductCount($item) ?></span>
                                 </h3>
-                                <div class="pt-1 px-1 mb-2">
+                                <div class="pt-2 px-1 mb-1">
                                     <?php foreach ($item['plays'] as $k => $play) {
                                         if ($play !== false){ ?>
-                                            <div class="d-flex align-items-baseline " style="line-height:25px;font-size:0.875rem;">
+                                            <div class="d-flex align-items-baseline pb-1" style="line-height:25px;font-size:0.875rem;">
                                                 <div class="mr-1">
                                                     <span style="display:inline-block; width:35px"><?= $play['label']?></span>
                                                 </div>
@@ -81,7 +97,7 @@ use CakeUtility\Hash;
                                 </div>
                             </div>
                         <?php
-													
+                            $cpt++;
                         }
                     ?>
                     </div>
@@ -92,27 +108,27 @@ use CakeUtility\Hash;
                                 <i class="fa fa-info pl-2"></i>
                              </span>
                         </div>
-                        <p class="mb-4" style="min-height: 110px;">
+                        <p class="mb-2" style="min-height: 80px; line-height: 28px">
                             <?php echo $result['result_summary']['displayed_price'];?>
                         </p>
-                        <div class="setup-wrapper">
-                            <div class="mb-1">
+                        <div class="setup-wrapper mb-1">
+                            <div class="mb-0">
                                 <?=$result['result_summary']['setup'];?>
                             </div>
                             <?php if (!empty($result['result_summary']['products_total_savings'])) { ?>
-                                <p class="total-savings modalClick cursor-pointer" data-toggle="modal" data-target="#modalTotalSavings">
+                                <p class="total-savings modalClick cursor-pointer mb-0" data-toggle="modal" data-target="#modalTotalSavings">
                                     <?= $result['result_summary']['products_total_savings'] ?>
                                     <span class="position-absolute">
                                         <i class="fa fa-info pl-2"></i>
                                     </span>
                                 </p>
                             <?php } ?>
-													<p class="position-relative sub-details-infos toggle-details toggle-details-<?= $params['id'] ?>">
-														<?= $result['result_summary']['phone_plug']?>
-													</p>
-													<p class="position-relative sub-details-infos toggle-details toggle-details-<?= $params['id'] ?>">
-														<?=$result['result_summary']['max_activation_time'];?>
-													</p>
+                            <p class="position-relative sub-details-infos toggle-details toggle-details-<?= $params['id'] ?>">
+                                <?= $result['result_summary']['phone_plug']?>
+                            </p>
+                            <p class="position-relative sub-details-infos toggle-details toggle-details-<?= $params['id'] ?>">
+                                <?=$result['result_summary']['max_activation_time'];?>
+                            </p>
 													
                         </div>
 
