9e96bac9600f44ed70dfaa539e1b12d164ff8e9d
valentin-rocher <valentin.rocher@opengraphy.com>
2024-03-20 16:50:44 +0100

    #2995 fix card

--- src/AstelShared/View/operator/products_listing_cards.php
+++ src/AstelShared/View/operator/products_listing_cards.php
@@ -34,6 +34,7 @@ use CakeUtility\Hash;
         ],
     ];
 */
+
 ?>
 
 <div class="container px-0 toggleProductListingDetails__container" id="toggleProductListingDetails__container_<?= $params['id'] ?>">
@@ -43,10 +44,10 @@ use CakeUtility\Hash;
     </h2>
     <div class="btn btn-outline-secondary text-uppercase cursor-pointer d-flex justify-content-center text-nowrap toggleProductListingDetails__button" id="toggle-product-listing-button-<?= $params['id'] ?>" onclick="toggleProductListingCards('<?= $params['id'] ?>')">
       <div class="details-hidden">
-        <?= self::getTranslation(['cake' => 'CompareAstelBe', 'front' => 'product'], 'switch_details', $this->version)?>&nbsp;<i class="fa fa-chevron-down ml-2" aria-hidden="true"></i>
+        <?= self::getTranslation(['cake' => 'CompareAstelBe', 'front' => 'product'], 'switch_resume', $this->version) ?>&nbsp;<i class="fa fa-chevron-up ml-2" aria-hidden="true"></i>
       </div>
       <div class="details-visible">
-        <?= self::getTranslation(['cake' => 'CompareAstelBe', 'front' => 'product'], 'switch_resume', $this->version)?>&nbsp;<i class="fa fa-chevron-up ml-2" aria-hidden="true"></i>
+        <?= self::getTranslation(['cake' => 'CompareAstelBe', 'front' => 'product'], 'switch_details', $this->version) ?>&nbsp;<i class="fa fa-chevron-down ml-2" aria-hidden="true"></i>
       </div>
     </div>
   </div>
@@ -61,40 +62,20 @@ use CakeUtility\Hash;
           <div class="result-index ml-2">
             <?= $result['result_index'] ?>
           </div>
-          <?php } ?>
-          <div class="mb-0 pb-0 rounded" style="background-color: #f5f5f5">
-
-            <?php
-                                // Display brand name only if 1st product , and also 2dn result if multi brand result
-                                if($cpt == 1 || ($cpt == 2 && $params['id'] == 'view_multi_brand')) { 
-                                    $productTitles = $result['result_summary']['product_titles'][$item['brand_name']];
-                                  ?>
-            <div class="titleproduct-logo-brand mb-n3 p-2 mb-0">
-              <img class="w-100" src="<?= $item['brand_logo'] ?>" alt="<?= $item['brand_name'] ?>" title="<?= $productTitles ?>">
+        <?php } ?>
+        <div class="px-2 pt-1 pb-2 rounded-lg d-flex h-100 flex-column justify-content-between" style="box-shadow: 2px 0rem 1.2rem rgba(0,0,0,.35)!important">
+          <?php if ($cashback) { ?>
+            <div class="mt-n3 ml-3 py-0 px-3 shadow cursor-pointer position-absolute rounded-sm plugin-hidden-optional-element cashback-amount modalClick " data-toggle="modal" data-target="#pluginModalCashback" style="color:#fff; background-color: #f23078; top:2px; height:32px; line-height: 32px; right: 0.75rem; font-size: 0.9rem;">
+              <?= $cashback ?> <i class="fa fa-info pl-1" style="font-size:1rem"></i>
             </div>
-
-            <?php } ?>
-            <?php if ($item['product_sheet_url'] != ''){ ?>
-            <a class="gtm-product-detail-link" href="<?= $item['product_sheet_url'] ?>"
-              title="<?= $item['name'] ?>" target="_blank" data-name="<?= $item['short_name']  ?>"
-              data-brand="<?= $item['brand_name'] ?>">
-              <?php } ?>
-              <h3 class="px-1 pt-3 mb-n2 d-flex justify-content-between" style="min-height: 46px; font-size: 1.1rem;"
-                <?= ($cpt == 1 ? 'style="min-height: 46px; font-size: 1.1rem;"' : '') ?>>
-                <span class="text-<?= $item['brand_slug']; ?> font-weight-bold">
-                  <?= $item['short_name']; ?>
-                </span>
-                <span class="font-weight-bold" style="1.2rem;"><?= self::getDisplayedProductCount($item) ?></span>
-              </h3>
-              <?php if ($item['product_sheet_url'] != ''){ ?>
-            </a>
-            <?php } ?>
-            <div class="px-1 mb-1 mt-2">
-              <?php foreach ($item['plays'] as $k => $play) {
-                                        if ($play !== false){ ?>
-              <div class="d-flex align-items-baseline pb-1" style="line-height:25px;font-size:0.875rem;">
-                <div class="mr-1 pt-2">
-                  <span style="display:inline-block; width:35px"><?= $play['label']?></span>
+          <?php } ?>
+          <div class="<?= $cashback ? 'mt-4' : 'mt-1' ?>">
+            <?php
+            $cpt = 1; // To display "+"
+            foreach ($result['products'] as $key => $item) {
+              if ($cpt > 1) { ?>
+                <div class="w-100 text-center mb-1">
+                  <i class="fa fa-plus" style="color:#878787" aria-hidden="true"></i>
                 </div>
               <?php } ?>
               <div class="mb-0 pb-0 rounded" style="background-color: #f5f5f5">
