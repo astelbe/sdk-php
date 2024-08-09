08c31c013c44f4c2cda23397fdc06060b0de0df3
Michael Godefroid <michael.godefroid@opengraphy.com>
2024-08-09 10:34:08 +0200

    #304 remove get First

--- src/AstelSDK/API/Testimonial.php
+++ src/AstelSDK/API/Testimonial.php
@@ -15,18 +15,4 @@ class Testimonial extends APIModel
 
     return $query->exec();
   }
-
-  protected function getFirst(array $params = [])
-  {
-    $query = $this->newQuery();
-    $id = Hash::get($params, 'id');
-    if ($id === null || !is_numeric($id)) {
-      return false;
-    }
-    unset($params['id']);
-    $query->setUrl('v2_00/testimonial/' . $id);
-    $query->addGETParams($params);
-
-    return $query->exec();
-  }
 }
