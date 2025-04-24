d6216a98bf776c71584fc909fc86fb7c66fff98d
valentin-rocher <valentin.rocher@opengraphy.com>
2025-04-24 13:48:18 +0200

    #3478 separate key

--- src/AstelSDK/WebIntegration/OrderForm.php
+++ src/AstelSDK/WebIntegration/OrderForm.php
@@ -29,7 +29,7 @@ class OrderForm extends AbstractWebIntegration {
     $order_form_js = [
       'https://files' . $this->context->getEnv() . '.astel.be/DJs/astelContentInjector.js?v=' . $this->context->getVersion(),
       'https://order' . $this->context->getEnv() . '.astel.be/orderForms/inject.js?v=' . $this->context->getVersion(),
-      'https://maps.googleapis.com/maps/api/js?key=AIzaSyD8NIuA9OgEe-UvCB5ITr_83hucmvukkrs&libraries=places&loading=async',
+      'https://maps.googleapis.com/maps/api/js?key=' . $this->context->apiKeyGooglePlace . '&libraries=places&loading=async',
     ];
     return array_merge($typeahead_js, $order_form_js);
   }
