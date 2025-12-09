cbef09366d7e98e86901ca83206dabd3649b8ae5
valentin-rocher <valentin.rocher@opengraphy.com>
2025-12-09 09:29:27 +0100

    #3568 Add Google Place Autocomplete integration and translations

--- src/AstelShared/GooglePlaceAutocomplete.php
+++ src/AstelShared/GooglePlaceAutocomplete.php
@@ -5,22 +5,29 @@ namespace AstelShared;
 use AstelSDK\Utils\Singleton;
 use CakeUtility\Hash;
 use AstelSDK\AstelContext;
+use AstelShared\Translate\Translate;
 
 class GooglePlaceAutocomplete extends Singleton {
 
 
-  public function getJsList() {
+  public function getJsList($options = []) {
     $Context = AstelContext::getInstance();
 
-    return [
+    $scripts = [
       'https://maps.googleapis.com/maps/api/js?key=' . $Context->apiKeyGooglePlace . '&libraries=places',
       'https://files' . $Context->getEnv() . '.astel.be/DJs/google_place_autocomplete.js?v=' . $Context->getVersion(),
     ];
+
+    // This file is already included in Order Form
+    if (isset($options['include_inject.js']) && $options['include_inject.js'] === true) {
+      $scripts[] = 'https://order' . $Context->getEnv() . '.astel.be/orderForms/inject.js?v=' . $Context->getVersion();
+    }
+    return $scripts;
   }
 
-  public function getJsScripts() {
+  public function getJsScripts($options = []) {
     $out = '';
-    foreach ($this->getJsList() as $js) {
+    foreach ($this->getJsList($options) as $js) {
       $out .= '<script type="text/javascript" src="' . $js . '"></script>';
     }
 
@@ -43,7 +50,7 @@ class GooglePlaceAutocomplete extends Singleton {
 ?>
     <div class="form-group d-block google-place-autocomplete">
       <label for="email" class="control-label col-md-0">
-        <?= __d('OrderAstelBe', 'autocomplete_address_label', true) ?>
+        <?= Translate::get('autocomplete_address_label') ?>
       </label>
       <div id="autocomplete_wrapper<?= $id ?>" class="mb-2">
       </div>
@@ -73,15 +80,18 @@ class GooglePlaceAutocomplete extends Singleton {
           ],
 
         ] as $addressComponent
-      ) { ?>
+      ) { 
+        
+        ?>
         <div class="d-flex align-items-baseline flex-column flex-sm-row my-1">
           <div>
 
             <span class="text-nowrap">
-              <?= __d('OrderAstelBe', $addressComponent['label']) ?>
+              <?= Translate::get($addressComponent['label']) ?>
             </span>
             <span id="<?= $id_ . $addressComponent['varName'] ?>_status" class="pl-1">
-              <?php if (!empty($addressComponent['value'])) { ?>
+              <?php 
+              if (!empty($addressComponent['value'])) { debug('ok'); ?>
                 <?= $addressComponent['value'] ?> <i class="fa fa-check" style="color:#28a745ad;"></i>
               <?php } else { ?>
                 <i class="fa fa-close" style="color:#ce0000d1;"></i>
@@ -91,16 +101,22 @@ class GooglePlaceAutocomplete extends Singleton {
           <?php
           // display a street number or postal code field if not found by google API to let user fill it manually
           foreach (['street_number', 'postal_code'] as $field) {
-            if ($addressComponent['varName'] == $field) { ?>
+            $name = (isset($options['order_context']) && $options['order_context'] === false) 
+            ? $id_ . $field . '_override' 
+            : 'data[OrderRequest][' . $id_ . $field . '_override]';
+            if ($addressComponent['varName'] == $field) { 
+              $value = isset($data[$field]) ? htmlspecialchars($data[$field]) : '';
+              ?>
               <input
-                name="data[OrderRequest][<?= $id_ . $field ?>_override]"
+                name="<?= $name ?>"
                 id="<?= $id_ . $field ?>_override"
+                value="<?= $value ?>" 
                 class="ml-2 validate-field required input-not-valid d-none form-control"
                 autocomplete="unknown"
                 data-validate="<?= $field == 'street_number' ? 'latinCharNum' : 'postalCode' ?>"
                 maxlength="15"
                 type="text"
-                placeholder="<?= __d('OrderAstelBe', $field . ' override placeholder'); ?>" style="width: 250px;">
+                placeholder="<?= Translate::get($field . ' override placeholder'); ?>" style="width: 250px;">
           <?php }
           } ?>
         </div> <!-- Validation status -->
@@ -112,7 +128,8 @@ class GooglePlaceAutocomplete extends Singleton {
 
     // add hidden address fields if necessary
     if ($options['use_hidden_fields']) {
-      $html .= $this->hiddenAddressInputFields();
+      $options['data'] = $data;
+      $html .= $this->hiddenAddressInputFields($options);
     }
     return $html;
   }
@@ -122,16 +139,22 @@ class GooglePlaceAutocomplete extends Singleton {
    * Used for frontend. 
    * In Order form those fields are already in the template to handle customer data
    */
-  private function hiddenAddressInputFields() {
+  private function hiddenAddressInputFields($options = []) {
 
     $html = '';
 
     foreach (['street1', 'street_number', 'postal_code', 'city'] as $input) {
 
+      $name = (isset($options['order_context']) && $options['order_context'] === false) 
+        ? $input 
+        : 'data[OrderRequest][' . $input . ']';
+
+      $value = isset($options['data'][$input]) ? htmlspecialchars($options['data'][$input]) : '';
+
       $html .= '<input 
-        name="data[OrderRequest][' . $input . ']" 
+        name="' . $name . '" 
         id="' . $input . '" 
-        value="" 
+        value="' . $value . '" 
         class="validate-field required form-control input-complete" 
         autocomplete="unknown" 
         data-validate="latinCharNum" 
